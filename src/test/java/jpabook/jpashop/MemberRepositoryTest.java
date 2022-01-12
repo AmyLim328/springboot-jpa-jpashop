99141dd1005ba83d82f14dd25b3489a21546e9b5
Amy Lim <amylim32897@gmail.com>
2022-01-12 16:52:39 +0900

    member repository test

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("memberA");
+
+        // when
+        Long savedId = memberRepository.save(member);
+        Member foundMember = memberRepository.find(savedId);
+
+        // then
+        assertThat(foundMember.getId()).isEqualTo(member.getId());
+        assertThat(foundMember.getName()).isEqualTo(member.getName());
+        assertThat(foundMember).isEqualTo(member);
+        System.out.println("foundMember == member: " + (foundMember == member));
+    }
+}
