a0497865e510a8601ffba0324c8fa5d3afd40b1c
Amy Lim <amylim32897@gmail.com>
2022-01-13 19:06:33 +0900

    member repository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor // SpringDataJPA에서는 이렇게 사용 가능 // service와 일관성 있게 이렇게 사용
+public class MemberRepository {
+
+//    @PersistenceContext
+//    private EntityManager em;
+
+    private final EntityManager em; // SpringDataJPA에서는 이렇게 사용 가능
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
