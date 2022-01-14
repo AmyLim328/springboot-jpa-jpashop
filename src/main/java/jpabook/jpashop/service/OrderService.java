05c87a4992b90ddaaff83cfdae101f60ed0591e7
Amy Lim <amylim32897@gmail.com>
2022-01-14 16:40:54 +0900

    order service

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,72 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Delivery;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // 엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // 배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress()); // 보통 입력한 주소로 배송을 하지만 예제를 간단하게 하기 위해 회원의 주소로 배송
+
+        // 주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count); // 예제를 단순화하기 위해 상품 한 종류만 넘기도록 제약
+
+//        OrderItem orderItem1 = new OrderItem(); // protected 생성자 때문에 오류 뜸 // 제약을 걸어둔 셈
+        // 직접 생성하면 안되고 다른 스타일로 생성해야 하는구나 알게 된다
+        // 제약하는 스타일로 코드를 작성해야 좋은 설계와 유지보수로 끌어갈 수 있다
+
+        // 주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // 주문 저장
+        orderRepository.save(order);
+
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    public void cancelOrder(Long orderId) {
+        // 주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+
+        // 주문 취소
+        order.cancel();
+    }
+
+    // 검색
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+
+}
