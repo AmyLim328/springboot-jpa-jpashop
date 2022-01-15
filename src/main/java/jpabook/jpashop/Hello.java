8dba7bfd190332d26b933d0b026c1dbe7af3b58d
Amy Lim <amylim32897@gmail.com>
2022-01-15 23:48:32 +0900

    hello controller

--- src/main/java/jpabook/jpashop/Hello.java
+++ src/main/java/jpabook/jpashop/Hello.java
@@ -2,8 +2,21 @@ package jpabook.jpashop;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter @Setter
 public class Hello {
     private String data;
+
+    @Controller
+    public static class HelloController {
+
+        @GetMapping("hello")
+        public String hello(Model model) {
+            model.addAttribute("data", "hello!");
+            return "hello";
+        }
+    }
 }
