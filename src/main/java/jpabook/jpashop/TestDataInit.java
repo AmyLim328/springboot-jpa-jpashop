ea082b3b029734f010ca40cbd42206da3999d632
Amy Lim <amylim32897@gmail.com>
2022-01-28 02:34:13 +0900

    TODO test data init

---
+++ src/main/java/jpabook/jpashop/TestDataInit.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        // ???????
+
+    }
+}
