0065d3806dc554d6546660cb8c896c579f9ddfa6
Amy Lim <amylim32897@gmail.com>
2022-01-27 23:32:27 +0900

    item service

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,15 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity ) { // 예제에서는 book만 사용하기로 했으니
+        Item findItem = itemRepository.findOne(itemId);
+        // findItem.change(price, name, stockQuantity); // 예제라서 setter 사용한 거지 원래는 의미있는 메서드 생성해서 업데이트 해주어야 함 (추적 가능하도록)
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
     }
 
     public List<Item> findItems() {
